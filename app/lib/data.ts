aaea463fc99f5b8490d9112b1425693e40d8dcd9
Luis Corales <luisaugustocorales@hotmail.com>
2024-03-13 20:21:50 -0500

    chapter 14

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -156,7 +156,6 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   noStore();
-
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -173,6 +172,8 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+
+    console.log(invoice); // Invoice is an empty array []
 
     return invoice[0];
   } catch (error) {
